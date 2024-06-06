18eafd7ed9a33a0edb0312edcef9fd55e877738f
shakil ahmed <shakiltha338@gmail.com>
2024-06-06 14:17:33 +0600

    why choose us for mobile complete

--- src/components/WhyChoose.jsx
+++ src/components/WhyChoose.jsx
@@ -7,42 +7,45 @@ const WhyChoose = () => {
   return (
     <div>
       <div>
-        <section>Why choose e-learn</section>
-        <p>
+        <section className="flex justify-center">
+          <h2 className="roboto-bold text-2xl md:text-4xl pl-2 border-l-2 border-l-[#024547]">
+            Why choose e-learn
+          </h2>
+        </section>
+        <p className="leto-regular text-lg text-center">
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor Lorem ipsum dolor sit amet
         </p>
       </div>
       <div>
-        <section>
+        <section className="mt-4 ml-3">
           <img src={icon1} alt="" />
-          <h5>Course accessibility</h5>
-          <p>
+          <h5 className="roboto-medium text-2xl mt-2">Course accessibility</h5>
+          <p className="lato-regular text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quasi
             omnis maxime dicta.
           </p>
         </section>
-        <section>
+        <section className="mt-4 ml-3">
           <img src={icon2} alt="" />
-          <h5>Schedule learning</h5>
-          <p>
+          <h5 className="roboto-medium text-2xl mt-2">Schedule learning</h5>
+          <p className="lato-regular text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quasi
             omnis maxime dicta.
           </p>
         </section>
-        <section>
+        <section className="mt-4 ml-3">
           <img src={icon3} alt="" />
-          <h5>Record sessions</h5>
-          <p>
+          <h5 className="roboto-medium text-2xl mt-2">Record sessions</h5>
+          <p className="lato-regular text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quasi
             omnis maxime dicta.
           </p>
         </section>
-        s
-        <section>
+        <section className="mt-4 ml-4">
           <img src={icon4} alt="" />
-          <h5>Expert institutions</h5>
-          <p>
+          <h5 className="roboto-medium text-2xl mt-2">Expert institutions</h5>
+          <p className="lato-regular text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quasi
             omnis maxime dicta.
           </p>
