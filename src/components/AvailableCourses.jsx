2ea908e6527bdff72f18d69b0149863fd80f14f9
shakil ahmed <shakiltha338@gmail.com>
2024-06-06 14:34:11 +0600

    available courses section for mobile complete

--- src/components/AvailableCourses.jsx
+++ src/components/AvailableCourses.jsx
@@ -6,40 +6,44 @@ import businessPresentation from "../assets/business-presentation-dashboard.png"
 
 const AvailableCourses = () => {
   return (
-    <div>
+    <div className="my-4">
       <div>
-        <section>Available Courses</section>
-        <p>
+        <section className="flex justify-center">
+          <h2 className="roboto-bold text-2xl md:text-4xl pl-2 border-l-2 border-l-[#024547]">
+            Available Courses
+          </h2>
+        </section>
+        <p className="leto-regular text-lg text-center">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut veniam
           in optio cum.
         </p>
       </div>
-      <div></div>
-      <div>
-        <img src={marketingIcon} alt="" />
-        <h5>Learn</h5>
-        <h3>Marketing</h3>
-      </div>
-      <div>
-        <img src={paintDesignIcon} alt="" />
-        <h5>Learn</h5>
-        <h3>Art & Design</h3>
-      </div>
-      <div></div>
-      <div>
-        <img src={codingIcon} alt="" />
-        <h5>Learn</h5>
-        <h3>Coding</h3>
-      </div>
-      <div>
-        <img src={musicIcon} alt="" />
-        <h5>Learn</h5>
-        <h3>Music</h3>
-      </div>
-      <div>
-        <img src={businessPresentation} alt="" />
-        <h5>Learn</h5>
-        <h3>Busniness</h3>
+      <div className="grid grid-cols-2 grid-rows-3 place-items-center mx-2 mt-3">
+        <div className=" p-2 shadow-lg w-[150px] h-[200px]">
+          <img src={marketingIcon} alt="" />
+          <h5>Learn</h5>
+          <h3>Marketing</h3>
+        </div>
+        <div className="p-2 shadow-lg w-[150px] h-[200px]">
+          <img src={paintDesignIcon} alt="" />
+          <h5>Learn</h5>
+          <h3>Art & Design</h3>
+        </div>
+        <div className="p-2 shadow-lg w-[150px] h-[200px]">
+          <img src={codingIcon} alt="" />
+          <h5>Learn</h5>
+          <h3>Coding</h3>
+        </div>
+        <div className="p-2 shadow-lg w-[150px] h-[200px]">
+          <img src={musicIcon} alt="" />
+          <h5>Learn</h5>
+          <h3>Music</h3>
+        </div>
+        <div className="p-2 shadow-lg w-[150px] h-[200px]">
+          <img src={businessPresentation} alt="" />
+          <h5>Learn</h5>
+          <h3>Busniness</h3>
+        </div>
       </div>
     </div>
   );
