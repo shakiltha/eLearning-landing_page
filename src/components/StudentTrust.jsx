0a4f51ad0ebd74d95f0d286b35d91efeaccf2aaf
shakil ahmed <shakiltha338@gmail.com>
2024-06-06 14:02:22 +0600

    student trust for mobile complete

--- src/components/StudentTrust.jsx
+++ src/components/StudentTrust.jsx
@@ -4,33 +4,33 @@ import microsoft from "../assets/microsoft.png";
 const StudentTrust = () => {
   return (
     <div>
-      <div>
+      <div className="flex justify-around my-5">
         <section>
-          <h4>23,000+</h4>
-          <p>Active Students</p>
+          <h4 className="roboto-bold text-2xl md:text-4xl">23,000+</h4>
+          <p className="lato-regular text-lg">Active Students</p>
         </section>
         <section>
-          <h4>2000+</h4>
-          <p>Courses</p>
+          <h4 className="roboto-bold text-2xl md:text-4xl">2000+</h4>
+          <p className="lato-regular text-lg">Courses</p>
         </section>
       </div>
       <div>
-        <p>
+        <p className="roboto-regular text-xl md:text-3xl mx-2 text-center">
           Trusted by 23,000+ happy Students are joining with us for achieve
           their goal
         </p>
-        <p>Also Featured in</p>
-        <section>
-          <section>
+        <p className="lato-regular text-lg text-center">Also Featured in</p>
+        <section className="flex gap-4 justify-center my-3">
+          <section className="flex gap-2">
             <img src={slackIcon} alt="" />
-            <span>slack</span>
+            <span className="roboto-bold">slack</span>
+          </section>
+          <section className="flex gap-2">
+            <img src={microsoft} className="w-[26px] h-[26px]" alt="" />
+            <span className="roboto-bold text-[#706D6E]">Microsoft</span>
           </section>
           <section>
-            <img src={microsoft} alt="" />
-            <span>Microsoft</span>
-          </section>
-          <section>
-            <p>facebook</p>
+            <p className="roboto-bold text-[#3B5998]">facebook</p>
           </section>
         </section>
       </div>
