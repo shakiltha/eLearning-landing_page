0ad3c095150a569cf68e19658fce2df7348c8cd0
shakil ahmed <shakiltha338@gmail.com>
2024-06-07 21:33:41 +0600

    pricing for large device done

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,18 +1,18 @@
 const Pricing = () => {
   return (
-    <div>
-      <section>
-        <section className="flex justify-center">
+    <div className="xl:flex xl:gap-5 xl:justify-center xl:mt-4">
+      <section className="xl:w-80">
+        <section className="flex justify-center xl:justify-start">
           <h3 className="roboto-bold text-2xl md:text-4xl pl-2 border-l-2 border-l-[#024547] text-center">
             Pricing
           </h3>
         </section>
-        <p className="leto-regular text-lg text-center">
+        <p className="leto-regular text-lg text-center xl:text-left">
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor Lorem ipsum dolor sit amet
         </p>
       </section>
-      <section className="md:flex">
+      <section className="md:flex xl:gap-4">
         <section className="border w-[311px] h-[380px] mx-auto shadow-2xl p-7 my-3 rounded-lg">
           <div className="flex justify-between">
             <span className="lato-regular">Advanced</span>
@@ -87,7 +87,7 @@ const Pricing = () => {
               </span>
             </section>
           </section>
-          <button className="w-60 h-14 bg-[#CC9D42] text-white rounded-xl mt-10 roboto-regular">
+          <button className="w-60 h-14 bg-[#CC9D42] text-white rounded-xl mt-10 xl:mt-20 roboto-regular">
             Buy now
           </button>
         </section>
